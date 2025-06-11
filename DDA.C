36804d04135eb59f52feb3c38c0b203031c72ce6
Sandesh Poudel <141824006+web-cipher-007@users.noreply.github.com>
2025-06-11 19:48:36 +0545

    DDA algorithm in C

--- DDA.C
+++ DDA.C
@@ -1,42 +1,39 @@
-#include<stdio.h>
-#include<conio.h>
-#include<graphics.h>
-#include<math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <math.h>
 
-void main()
-{
-int gd=DETECT,gm;
-int x1,y1,x2,y2,stepsize,dx,dy,i;
-float x,y,xinc,yinc;
-initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-printf("Digital Differential Line Drawing Algorithm\n");
-printf("Put the value of x1 and y1\n");
-scanf("%d%d",&x1,&y1);
-printf("Put the value of x2 and y2\n");
-scanf("%d%d",&x2,&y2);
+void main() {
+    int gd = DETECT, gm;
+    int x1, y1, x2, y2, steps;
+    float dx, dy, x, y, xinc, yinc;
 
-dx=x2-x1;
-dy=y2-y1;
-x=x1;
-y=y1;
-if(abs(dy)>abs(dx))
-{
-stepsize=abs(dy);
-}
-else
-{
-stepsize=abs(dx);
-}
-xinc=dx/(float)stepsize;
-yinc=dy/(float)stepsize;
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
 
-putpixel(x,y,RED);
-for(i=0;i<stepsize;i++)
-{
-x=x+xinc;
-y=y+yinc;
-putpixel((int)(x+0.5),(int)(y+0.5),RED);
-}
-getch();
-closegraph();
+    printf("Digital Differential Analyzer (DDA) Line Drawing Algorithm\n");
+
+    printf("Enter x1 and y1: ");
+    scanf("%d%d", &x1, &y1);
+    printf("Enter x2 and y2: ");
+    scanf("%d%d", &x2, &y2);
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+
+    steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    xinc = dx / steps;
+    yinc = dy / steps;
+
+    x = x1;
+    y = y1;
+
+    for (int i = 0; i <= steps; i++) {
+        putpixel((int)(x + 0.5), (int)(y + 0.5), RED);
+        x += xinc;
+        y += yinc;
+    }
+
+    getch();
+    closegraph();
 }
